fc79e8e8b309b3cf66f4bd705686c784d693544d
Raül Pérez <repejota@gmail.com>
2018-01-31 23:00:32 +0100

    simple socket server and main template

--- cmd/misto/main.go
+++ cmd/misto/main.go
@@ -2,14 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io"
 	"log"
+	"net/http"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/hashicorp/go.net/websocket"
 	"github.com/repejota/misto"
 	"golang.org/x/net/context"
 )
+
+func logHandler(ws *websocket.Conn) {
+	var err error
+
+	for {
+		var reply string
+
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println("Can't receive")
+			break
+		}
+
+		fmt.Println("Received back from client: " + reply)
+
+		msg := "Received:  " + reply
+		fmt.Println("Sending to client: " + msg)
+
+		if err = websocket.Message.Send(ws, msg); err != nil {
+			fmt.Println("Can't send")
+			break
+		}
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
 
 func main() {
 	ctx := context.Background()
@@ -41,9 +72,14 @@ func main() {
 		readers = append(readers, logs)
 	}
 
+	http.Handle("/logs", websocket.Handler(logHandler))
+	http.HandleFunc("/", handler)
+	go http.ListenAndServe(":7999", nil)
+
 	scanner := misto.NewConcurrentScanner(readers...)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		entry := scanner.Text()
+		fmt.Println(entry)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatalln(err)
